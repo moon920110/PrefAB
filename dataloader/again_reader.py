e3e52be1939355af6b47b6a4efcedfee63b3d5a5
moon920110 <moon920110@gmail.com>
2023-07-31 18:24:23 +0900

    Build dataloading process

--- dataloader/again_reader.py
+++ dataloader/again_reader.py
@@ -1,19 +1,70 @@
 import os
+
 import pandas as pd
+from tqdm import tqdm
 
 
 class AgainReader:
-    def __init__(self, data_path='/data/AGAIN'):
-        data_path = data_path
+    def __init__(self, config=None):
+        self.data_path = config['data']['path']
+        self.config = config
 
-        print(f'data from {os.path.join(data_path, "clean_data", "clean_data.csv")}')
-        self.again = pd.read_csv(os.path.join(data_path, 'clean_data', 'clean_data.csv'), encoding='utf-8', low_memory=False)
+        print(f'data from {os.path.join(self.data_path, "clean_data", "clean_data.csv")}')
+        # read csv without row index
+        self.again = pd.read_csv(os.path.join(self.data_path, 'clean_data', 'clean_data.csv'),
+                                 encoding='utf-8',
+                                 low_memory=False)
+        self.again.columns = [col.split(']')[-1] for col in self.again.columns]
+
+    def _prepare_ordinal_dataset(self, target, target_name):
+        if target == 'game':
+            again = self.game_info_by_name(target_name)
+        elif target == 'genre':
+            again = self.game_info_by_genre(target_name)
+        else:
+            again = self.again
+
+        # get arousal diff by frame interval
+        arousal_diff = again.groupby(['player_id', 'game'])['arousal'].diff()
+        # NaN and 0 to 0.5 negative to 0 positive to 1
+        pair_rank_label = arousal_diff.apply(lambda x: 0.5 if pd.isna(x) or x == 0 else 0 if x < 0 else 1)
+
+        again['pair_rank_label'] = pair_rank_label
+        again = again.drop(columns=['arousal'])
+
+        return again
+
+    def prepare_sequential_ranknet_dataset(self):
+        data = self._prepare_ordinal_dataset(target='genre', target_name=self.config['train']['genre'])
+        x = []
+        total_iter = len(data['game'].unique()) * len(data['player_id'].unique())
+        pbar = tqdm(total=total_iter, desc='Preparing sequential dataset')
+        numeric_columns = data.select_dtypes(include=['number']).columns
+        # add game at the first
+        numeric_columns = ['game'] + list(numeric_columns)
+
+        for game in data['game'].unique():
+            for player in data['player_id'].unique():
+                pbar.update(1)
+                player_data = data[(data['game'] == game) & (data['player_id'] == player)]
+                if len(player_data) == 0:
+                    continue
+                video_name = f'{player}_{self.config["game_name"][game]}_{player_data["session_id"].unique()[0]}.h5'
+                video_full_path = os.path.join(self.data_path, self.config['data']['vision']['frame'], video_name)
+
+                player_data = player_data.sort_values('time_index')
+                pads = pd.concat([player_data.iloc[0]] * 3, axis=1).T
+                player_data = pd.concat([pads, player_data], ignore_index=True)
+
+                x.append([player_data, video_full_path])
+
+        return x, numeric_columns
 
     def game_info_by_genre(self, genre):
-        return self.again[self.again['[control]genre'] == genre].dropna(axis=1, how='any')
+        return self.again[self.again['genre'] == genre].dropna(axis=1, how='any')
 
     def game_info_by_name(self, game_name):
-        return self.again[self.again['[control]game'] == game_name].dropna(axis=1, how='any')
+        return self.again[self.again['game'] == game_name].dropna(axis=1, how='any')
 
     def common_features(self):
         shooter_games = self.game_info_by_genre('Shooter')
@@ -23,13 +74,13 @@ class AgainReader:
         return list(set(shooter_games.columns) & set(platform_games.columns) & set(racing_games.columns))
 
     def unique_game_info(self):
-        return self.again.drop_duplicates(subset='[control]game', keep='first')
+        return self.again.drop_duplicates(subset='game', keep='first')
 
-    def available_feature_names_by_game(self, game_name):
+    def available_feature_names(self, target, target_name):
         game_info = self.unique_game_info()
-        return game_info[game_info['[control]game'] == game_name].dropna(axis=1, how='any').columns
+        return game_info[game_info[target] == target_name].dropna(axis=1, how='any').columns
 
 
 if __name__ == "__main__":
     again_reader = AgainReader()
-    print(again_reader.available_feature_names_by_game('Heist!'))
+    print(again_reader.load_ranknet_train_eval())
---
+++ dataloader/pair_loader.py
@@ -0,0 +1,93 @@
+import numpy as np
+import h5py
+import torch
+from torch.utils.data import Dataset
+from torchvision import transforms
+from tqdm import tqdm
+
+from dataloader.again_reader import AgainReader
+
+
+class PairLoader(Dataset):
+    def __init__(self, config):
+        self.dataset, self.numeric_columns = AgainReader(config).prepare_sequential_ranknet_dataset()
+        self.config = config
+
+        self.x_img_pairs = []
+        self.x_meta_pairs = []
+        self.y = []
+
+        self.transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=self.config['data']['img_mean'], std=self.config['data']['img_std'])
+        ])
+
+        self.sequential = self.config['data']['sequential']
+        if self.sequential:
+            self.init_sequence_dataset()
+        else:
+            self.init_dataset()
+
+    def init_sequence_dataset(self):
+        for x, img_path in tqdm(self.dataset, desc=f'Preparing sequential dataset'):
+            for game in x['game'].unique():
+                for player in x['player_id'].unique():
+                    player_data = x[(x['game'] == game) & (x['player_id'] == player)]
+                    if len(player_data) == 0:
+                        continue
+
+                    for idx in range(5, len(player_data)):
+                        seq = player_data.iloc[idx-5:idx]  # stack 5 frames (0~3), (1~4) pair
+                        img_data = [img_path, seq['time_index'].values, seq['time_stamp'].values]
+                        y = seq['pair_rank_label'].values[-1]  # label of the last frame
+                        seq['game'] = self.config['game_numbering'][self.config['train']['genre']][game]
+                        seq = seq.loc[:, self.numeric_columns]
+                        seq = seq.drop(columns=['pair_rank_label']).values.astype(np.float32)
+
+                        self.x_img_pairs.append(img_data)
+                        self.x_meta_pairs.append(seq)
+                        self.y.append(y)
+
+    def init_dataset(self):
+        for x, img_path in tqdm(self.dataset, desc=f'Preparing dataset'):
+            for game in x['game'].unique():
+                for player in x['player_id'].unique():
+                    player_data = x[(x['game'] == game) & (x['player_id'] == player)]
+                    if len(player_data) == 0:
+                        continue
+                    for _, row in player_data.iterrows():
+                        img_data = [img_path, row['time_index'], row['time_stamp']]
+                        y = row['pair_rank_label']
+                        row['game'] = self.config['game_numbering'][self.config['train']['genre']][game]
+                        row = row.loc[self.numeric_columns]
+                        row = row.drop(columns=['pair_rank_label']).astype(np.float32)
+
+                        self.x_img_pairs.append(img_data)
+                        self.x_meta_pairs.append(row)
+                        self.y.append(y)
+
+    def __len__(self):
+        return len(self.y)
+
+    def __getitem__(self, idx):
+        img_data = self.x_img_pairs[idx]
+        meta = self.x_meta_pairs[idx]
+        y = self.y[idx]
+
+        img_path, time_indices, time_stamps = img_data
+        if self.sequential:
+            with h5py.File(img_path, 'r') as f:
+                # (sequence, height, width, channel)
+                compare_frames = torch.stack(
+                    [self.transform(np.array(f[f'frames/{time_index}_{time_stamp}']).astype(np.float32))
+                     for time_index, time_stamp in zip(time_indices[:4], time_stamps[:4])])
+                # (sequence, height, width, channel) to (sequence, channel, height, width) // open cv BGR format 0~255
+                main_frames = torch.stack(
+                    [self.transform(np.array(f[f'frames/{time_index}_{time_stamp}']).astype(np.float32))
+                     for time_index, time_stamp in zip(time_indices[1:], time_stamps[1:])])
+            return compare_frames, torch.tensor(meta[:4]), main_frames, torch.tensor(meta[1:]), torch.tensor(y)
+
+        else:
+            with h5py.File(img_path, 'r') as f:
+                frames = self.transform(np.array(f[f'frames/{time_indices}_{time_stamps}']))
+            return frames, torch.tensor(meta), torch.tensor(y)
