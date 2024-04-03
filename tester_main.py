b2c058fcb25f56d09401554c3e771d72e533b81c
moon920110 <moon920110@gmail.com>
2024-04-03 17:54:05 +0900

    Add finding peak and valley function and tester main file

---
+++ tester_main.py
@@ -0,0 +1,24 @@
+from dataloader.again_reader import AgainReader
+import yaml
+
+from utils.stats import find_significant_peaks_and_valleys
+
+if __name__ == '__main__':
+    with open('config/config.yaml') as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+        config['data']['path'] = 'data/'
+    game = 'Shootout'
+    print(f'read data {game}')
+    again_reader = AgainReader(config)
+    # data = again_reader.game_info_by_name(game)
+    data = again_reader.game_info_by_name('Shootout')
+    print(f'len data: {len(data)}')
+
+    sessions = data['session_id'].unique()
+    for i, session in enumerate(sessions):
+        print(f'session: {session}')
+        session_data = data[data['session_id'] == session]
+        find_significant_peaks_and_valleys(session_data['arousal'].values)
+
+        if i > 20:
+            break
--- utils/stats.py
+++ utils/stats.py
@@ -1,9 +1,9 @@
 import pandas as pd
-import yaml
 import dtw
 import numpy as np
 import matplotlib.pyplot as plt
 from tslearn.clustering import TimeSeriesKMeans, silhouette_score
+from scipy.signal import find_peaks
 
 
 def calc_correlation_per_player(data):
@@ -77,18 +77,41 @@ def get_dtw_cluster(data, config):
     return session_cluster
 
 
-if __name__ == "__main__":
-    from dataloader.again_reader import AgainReader
-    with open('../config/config.yaml') as f:
-        config = yaml.load(f, Loader=yaml.FullLoader)
-    game = 'Shootout'
-    print(f'read data {game}')
-    again_reader = AgainReader(config)
-    # data = again_reader.game_info_by_name(game)
-    data = again_reader.game_info_by_name('Shootout')
-    print(f'len data: {len(data)}')
+# TODO: peak를 구하는 근거 찾아서 정교화할 것, 현재 top peak 찾으면 탐지가 안 되는 애들이 좀 있음.
+def find_significant_peaks_and_valleys(data, threshold=0.1, prominence=None):
+    smoothed_data = np.convolve(data, np.ones(10) / 10, mode='same')
+    peaks, _ = find_peaks(smoothed_data, prominence=prominence)
+    valleys, _ = find_peaks(-smoothed_data, prominence=prominence)
 
-    get_dtw_cluster(data, config)
+    peak_prominences = smoothed_data[peaks] - np.min(smoothed_data)
+    valley_depths = np.max(smoothed_data) - smoothed_data[valleys]
+
+    peak_threshold = int(len(peaks) * threshold)
+    valley_threshold = int(len(valleys) * threshold)
+
+    top_peaks = peaks[np.argsort(peak_prominences)[-peak_threshold:]]
+    top_valleys = valleys[np.argsort(valley_depths)[-valley_threshold:]]
+
+    # show peaks and valleys
+    plt.plot(smoothed_data)
+    plt.plot(top_peaks, smoothed_data[top_peaks], "x")
+    plt.plot(top_valleys, smoothed_data[top_valleys], "x")
+    plt.show()
+
+
+# if __name__ == "__main__":
+#     from dataloader.again_reader import AgainReader
+#     with open('../config/config.yaml') as f:
+#         config = yaml.load(f, Loader=yaml.FullLoader)
+#     game = 'Shootout'
+#     print(f'read data {game}')
+#     again_reader = AgainReader(config)
+#     # data = again_reader.game_info_by_name(game)
+#     data = again_reader.game_info_by_name('Shootout')
+#     print(f'len data: {len(data)}')
+#
+#     find_significant_peaks_and_valleys(data['arousal'].values)
+    # get_dtw_cluster(data, config)
 
     #######################################################################
     # feature_names = again_reader.available_feature_names_by_game(game)
