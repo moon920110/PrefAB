42e56b41f48c07d9a9054a87d3f29718f2444aac
moon920110 <moon920110@gmail.com>
2025-02-07 11:07:05 +0900

    Fix tester

--- executor/tester.py
+++ executor/tester.py
@@ -42,13 +42,13 @@ class RanknetTester:
         model.load_state_dict(
             torch.load(
                 os.path.join(self.config['test']['save_dir'],
-                             f'ranknet_{self.config["train"]["exp"]}_best.pth')
+                             f'ranknet_{self.config["test"]["exp"]}_best.pth')
             )
         )
         model.to(self.device)
         model.eval()
 
-        self.logger.info(f'Model loaded from {self.config["test"]["save_dir"]}/{self.config["train"]["exp"]}_best.pth')
+        self.logger.info(f'Model loaded from {self.config["test"]["save_dir"]}/{self.config["test"]["exp"]}_best.pth')
         self.logger.info(f'Metadata will be saved at {label_path}')
         with torch.no_grad():
             # model save if validation accuracy is the best
@@ -68,13 +68,13 @@ class RanknetTester:
                 bios = []
                 batch_idx = 0
                 for data_idx in tqdm(range(start_idx, end_idx), desc=f'Reconstructing Graphs {i}/{len(indices)}'):
-                    img, feature, bio, y = self.test_dataset[data_idx]
+                    img, feature, bio, y, cluster = self.test_dataset[data_idx]
                     imgs.append(img)
                     features.append(feature)
                     bios.append(bio)
                     labels.append(y)
 
-                    metadata.append([bio[:, 0], bio[:, 1], bio[:, 2],bio[:, 3], bio[:, 4], bio[:, 5], bio[:, 6], bio[:, 7]])
+                    metadata.append([bio[:, 0], bio[:, 1], bio[:, 2],bio[:, 3], bio[:, 4], bio[:, 5], bio[:, 6], bio[:, 7], cluster])
 
                     if len(imgs) == self.batch_size or data_idx == end_idx - 1:
                         batch_idx += 1
@@ -82,7 +82,7 @@ class RanknetTester:
                         features = torch.stack(features).to(self.device)
                         bios = torch.stack(bios).to(self.device)
 
-                        o, _, z = model(imgs, features, bios, test=True)
+                        o, _, _, z = model(imgs, features, bios, test=True)
                         o = o.cpu().detach().numpy()
                         outputs.extend(o)
 
@@ -114,19 +114,9 @@ class RanknetTester:
             embeddings = np.vstack(embeddings)
             writer.add_embedding(
                 torch.tensor(embeddings),
-                metadata_header=['Age', 'Gender', 'Frequency', 'Gamer', 'PC', 'Mobile', 'Console', 'Genre'],
+                metadata_header=['Age', 'Gender', 'Frequency', 'Gamer', 'PC', 'Mobile', 'Console', 'Genre', 'Cluster'],
                 metadata=metadata,
                 # tag=f't-SNE Embeddings {i}',
             )
         if writer is not None:
             writer.close()
-
-    def _metric(self, y_pred, y_true, cutpoints):
-        _y_pred = y_pred.cpu().detach().numpy()
-        _y_pred = np.where(_y_pred < cutpoints[0], 0, np.where(_y_pred < cutpoints[1], 1, 2))
-
-        acc = accuracy_score(y_true.cpu().detach().numpy(), _y_pred)
-        cm = confusion_matrix(y_true.cpu().detach().numpy(), _y_pred, labels=[0, 1, 2])
-
-        return acc, cm
-
